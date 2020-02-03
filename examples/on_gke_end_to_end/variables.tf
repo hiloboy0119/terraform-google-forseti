d3f6a94086644c52c5eea4517f146ee7d2a29b79
Drew Goya <drew@videoamp.com>
2020-02-03 11:00:39 -0800

    Updating kubernetes version to avoid this issue with workloads:
    https://cloud.google.com/kubernetes-engine/docs/release-notes#january_24_2020

--- examples/on_gke_end_to_end/variables.tf
+++ examples/on_gke_end_to_end/variables.tf
@@ -137,7 +137,7 @@ variable "k8s_forseti_server_image_tag" {
 variable "kubernetes_version" {
   type        = string
   description = "The Kubernetes version of the masters. If set to 'latest' it will pull latest available version in the selected region."
-  default     = "1.15.7-gke.2"
+  default     = "1.15.8-gke.2"
 }
 
 variable "network" {
